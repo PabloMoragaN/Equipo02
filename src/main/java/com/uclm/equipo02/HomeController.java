f3fcca65719f7ccb9703a989159f210df63121bf
Pablo Moraga <32258291+PabloMoragaN@users.noreply.github.com>
2018-11-01 21:41:56 +0100

    update

--- src/main/java/com/uclm/equipo02/HomeController.java
+++ src/main/java/com/uclm/equipo02/HomeController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.uclm.equipo02.modelo.Fichaje;
 import com.uclm.equipo02.modelo.Usuario;
 import com.uclm.equipo02.persistencia.UsuarioDaoImplement;
 
@@ -26,7 +28,9 @@ import com.uclm.equipo02.persistencia.UsuarioDaoImplement;
 
 public class HomeController {
 	
-	
+	Usuario empleado = new Usuario();
+
+	Fichaje fichaje;
 
 	private final String usuarioServ = "usuario";
 	private final String usuario_login = "login";
@@ -121,7 +125,33 @@ public class HomeController {
 		System.out.println("Invalidamos la sesion: " + sesion);
 
 		return cambiarVista(usuario_login);
+	
 	}
+	
+	
+	@RequestMapping(value = "/abrirFichaje", method = RequestMethod.POST)
+	public ModelAndView abrirFichaje(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String hora, mensaje;
+		String fecha;
+		
+		
+		hora=(java.time.LocalTime.now()).toString();
+		fecha=(java.time.LocalDate.now()).toString();
+		
+		mensaje = "Has abierto to fichaje";
+		//nombre,fecha,hora,estado
+		fichaje = new Fichaje(empleado.getNombre(), fecha, hora,true);
+		return new ModelAndView("fichajes", "mensaje", mensaje);
+	} 
+
+	@RequestMapping(value = "/cerrarFichaje", method = RequestMethod.POST)
+	public ModelAndView cerrarFichaje(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String hora, mensaje;
+		hora=(java.time.LocalTime.now()).toString();
+		mensaje = "Has cerrado tu fichaje";
+		fichaje.cerrarFichaje(hora, empleado);
+		return new ModelAndView("home", "mensaje", mensaje);
+	} 
 	
 	
 }
